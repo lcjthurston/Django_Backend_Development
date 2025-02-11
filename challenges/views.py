a6f1fc71975a623a6d0e2394573987456caa418f
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-02-11 09:48:42 -0600

    Refactoring Folders/Code for Section 3

--- challenges/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here. (they process response - they're a function that takes a request and sends back a response)
-
-def index(request):
-    return HttpResponse("This works!")
--- monthly_challenges/manage.py
+++ monthly_challenges/manage.py
@@ -3,9 +3,10 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend_Django_Development.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monthly_challenges.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
