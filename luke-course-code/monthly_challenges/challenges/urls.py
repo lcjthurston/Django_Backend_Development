33bdea3a1d486b3b960ba46ae5fb2ffad9e2a2d0
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-02-12 12:37:23 -0600

    Added comments to urls.py

--- luke-course-code/monthly_challenges/challenges/urls.py
+++ luke-course-code/monthly_challenges/challenges/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+# List of URLs
 urlpatterns = [
+    # The path function maps urls to views.
+    # The angle brackets inside the quotes refer to a dynamic variable in the views.py file
+    # These angled bracket views are prepeded with types that specify which view function to call.
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
