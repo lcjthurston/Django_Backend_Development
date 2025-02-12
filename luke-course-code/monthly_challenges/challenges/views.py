49e779bbac06ac47d50d4b1b43390088e0e88ffc
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-02-11 22:41:28 -0600

    Removed Extra Code from Dynamic Path Segments

--- luke-course-code/monthly_challenges/challenges/views.py
+++ luke-course-code/monthly_challenges/challenges/views.py
@@ -4,18 +4,6 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
-
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes each day!")
-
-
-def march(request):
-    return HttpResponse("Learn Django for at least 20 minutes every day!")
-
-
 def monthly_challenge(request, month):
     challenge_text = None
     if month == 'january':
