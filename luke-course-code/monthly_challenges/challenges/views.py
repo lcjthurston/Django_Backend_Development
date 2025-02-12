ca797ac4e1024d95ace2ecd8d541c690db37eb4d
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-02-11 22:18:43 -0600

    Added Python Formatter

--- luke-course-code/monthly_challenges/challenges/views.py
+++ luke-course-code/monthly_challenges/challenges/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def january(request):
     return HttpResponse("Eat no meat for the entire month!")
+
 
 def february(request):
     return HttpResponse("Walk for at least 20 minutes each day!")
