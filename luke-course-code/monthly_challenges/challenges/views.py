d63fd9e5c4b9d7c52dd1efc89b0512d37ca8bd87
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-02-13 15:04:36 -0600

    Added Interpolation to HTML

--- luke-course-code/monthly_challenges/challenges/views.py
+++ luke-course-code/monthly_challenges/challenges/views.py
@@ -48,7 +48,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "month_name": month.capitalize(),
+            "text": challenge_text
+        })
         # string interpolation to inject values into text by preceding string with f
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
